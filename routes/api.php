664b74fb94c6d6f7905b1ba0577b5af2f6ca1e0f
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-05-21 23:20:46 +0800

    install api

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('employees', Api\EmployeeController::class);
+    Route::apiResource('attendance', Api\AttendanceController::class);
+    Route::apiResource('leaves',     Api\LeaveRequestController::class);
+    Route::apiResource('payroll',    Api\PayrollController::class);
+});

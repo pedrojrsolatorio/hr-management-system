af7aa9d6b50c68171e99bfbb07b92a1c716b8d9a
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-05-24 22:26:24 +0800

    comment out api controllers not yet implemented

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Api;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('employees', Api\EmployeeController::class);
-    Route::apiResource('attendance', Api\AttendanceController::class);
-    Route::apiResource('leaves',     Api\LeaveRequestController::class);
-    Route::apiResource('payroll',    Api\PayrollController::class);
+    // Route::apiResource('attendance', Api\AttendanceController::class);
+    // Route::apiResource('leaves',     Api\LeaveRequestController::class);
+    // Route::apiResource('payroll',    Api\PayrollController::class);
 });

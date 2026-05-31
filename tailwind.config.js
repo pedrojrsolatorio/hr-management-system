5b63f93b2c0c79111291036012b4de4a5626f1ce
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-05-31 17:55:13 +0800

    fix hover:bg-indigo-50 and hover:translate-x-0.5 by adding a safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
+
+    safelist: ["hover:bg-indigo-50", "hover:translate-x-0.5"],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },

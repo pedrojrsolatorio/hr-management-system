320ca17d2841dacd870af2ef93ef0166ca9835d8
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-05-31 23:27:06 +0800

    apply withTrashed() in user() to fix name on null error

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -31,7 +31,7 @@ class Employee extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
     public function department(): BelongsTo
     {

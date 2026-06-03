fed4eba2a234a26744fdf127d01a392af27a1c44
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-06-03 18:15:45 +0800

    udpate run() method to avoid duplication

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -13,13 +13,29 @@ class AdminUserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+    // public function run(): void
+    // {
+    //     $admin = User::create([
+    //         'name' => 'System Admin',
+    //         'email' => 'admin@hrms.com',
+    //         'password' => Hash::make('password'),
+    //     ]);
+    //     $admin->roles()->attach(Role::where('slug', 'admin')->first());
+    // }
+
+    // recommended version to avoid duplicates when seeding multiple times
     public function run(): void
     {
-        $admin = User::create([
-            'name' => 'System Admin',
-            'email' => 'admin@hrms.com',
-            'password' => Hash::make('password'),
-        ]);
-        $admin->roles()->attach(Role::where('slug', 'admin')->first());
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@hrms.com'],
+            [
+                'name' => 'System Admin',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        $adminRole = Role::where('slug', 'admin')->first();
+
+        $admin->roles()->syncWithoutDetaching([$adminRole->id]);
     }
 }

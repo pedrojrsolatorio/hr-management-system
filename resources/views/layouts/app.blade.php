899a37547649be2208e3e72b9e79ad46638f979a
Pedro Jr Solatorio <solatoriop@gmail.com>
2026-06-15 22:54:53 +0800

    add notification link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,7 +145,29 @@
         {{-- Main content --}}
         <div class="flex flex-1 flex-col">
             <header class="border-b border-gray-100 bg-white px-8 py-4">
-                {{ $header ?? '' }}
+                <div class="flex items-center justify-between">
+                    <div>{{ $header ?? '' }}</div>
+
+                    {{-- Notification Icon --}}
+                    <div class="relative">
+                        <a href="{{ route('notifications.index') }}"
+                            class="relative inline-flex rounded-full p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+
+                            {{-- Badge --}}
+                            @if (auth()->user()->unreadNotifications->count() > 0)
+                                <span
+                                    class="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-[10px] font-bold text-white">
+                                    {{ auth()->user()->unreadNotifications->count() }}
+                                </span>
+                            @endif
+                        </a>
+                    </div>
+                </div>
             </header>
             <main class="flex-1 overflow-y-auto">
                 {{ $slot }}
